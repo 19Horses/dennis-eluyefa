c80dca99b0519fa6859ad79f739772fcb24b0028
Cammy Docherty <cammy.docherty@klaviyo.com>
2026-01-30 13:13:44 +0000

    Pretty

--- frontend/src/pages/Work/index.tsx
+++ frontend/src/pages/Work/index.tsx
@@ -1,36 +1,43 @@
 import Layout from '../../components/Layout';
 import { Loading } from '../../components/Loading';
 import { useGetProjects } from '../../queries/useGetProjects';
-import { ProjectCard, ProjectImage, ProjectTitle, WorkContainer } from './styles';
+import {
+  ProjectCard,
+  ProjectImage,
+  ProjectTitle,
+  WorkContainer,
+} from './styles';
 
 function Work() {
-    const { data, isLoading, isError } = useGetProjects();
+  const { data, isLoading, isError } = useGetProjects();
 
-    if (isLoading) {
-        return <Loading />;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    if (isError) {
-        return (
-            <Layout>
-                <p>Error!</p>
-            </Layout>
-        );
-    }
-
+  if (isError) {
     return (
-        <Layout>
-            <WorkContainer>
-                {data?.map((project, index) => (
-                    <ProjectCard key={project._id} $index={index}>
-
-                        <ProjectImage src={project.images[0].asset?.url} alt={project.title} />
-                        <ProjectTitle>{project.title}</ProjectTitle>
-                    </ProjectCard>
-                ))}
-            </WorkContainer>
-        </Layout>
+      <Layout>
+        <p>Error!</p>
+      </Layout>
     );
+  }
+
+  return (
+    <Layout>
+      <WorkContainer>
+        {data?.map((project, index) => (
+          <ProjectCard key={project._id} $index={index}>
+            <ProjectImage
+              src={project.images[0].asset?.url}
+              alt={project.title}
+            />
+            <ProjectTitle>{project.title}</ProjectTitle>
+          </ProjectCard>
+        ))}
+      </WorkContainer>
+    </Layout>
+  );
 }
 
 export default Work;
